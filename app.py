8cf874f42e5885abf643ce6b0e70607c64ac1f9b
QUIZPAPA <michael.paparella42@gmail.com>
2026-09-09 20:18:10 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-```python
 import os
 import random
 from flask import Flask, render_template, request, redirect, session
@@ -216,4 +215,3 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
 
     app.run(host="0.0.0.0", port=port)
-```
